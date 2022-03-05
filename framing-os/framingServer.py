870e48e47e2c9462af89e67afabd01235566d9e1
Efrain Retana <eretana1@miners.utep.edu>
2022-03-05 16:59:54 -0700

    Include modularity for reading sockets, create client and server

--- framing-os/framingServer.py
+++ framing-os/framingServer.py
@@ -1,53 +1,43 @@
 #! /usr/bin/env python3
 
-# Framing server program
+# framing server program
 
-import socket, sys, re
-sys.path.append("../lib")       # for params
+import socket
+import sys
+import re
+import os
+import time
+
+import message_handler
+
+sys.path.append("../lib")  # for params
 import params
 
-
-# Sends a message through the given socket
-def send_message(s, message):
-    # Pack the first 4 bytes as length of message
-    message = struct.pack(">i", len(message)) + message
-    s.sendall(message)
-    
-
-def receive_message(s):
-    # Obtain message len
-    data_len = s.recv(4)
-
-    # No data len obtained
-    if not data_len:   
-        return None
-
-    # Create
-    data_len = struct.unpack
-
-    
 switchesVarDefaults = (
-    (('-l', '--listenPort') ,'listenPort', 50001),
-    (('-?', '--usage'), "usage", False), # boolean (set if present)
-    )
-
+    (('-l', '--listenPort'), 'listenPort', 50001),
+    (('-?', '--usage'), "usage", False),  # boolean (set if present)
+)
 
 progname = "framingserver"
 paramMap = params.parseParams(switchesVarDefaults)
 
 listenPort = paramMap['listenPort']
-listenAddr = ''       # Symbolic name meaning all available interfaces
+listenAddr = ''  # Symbolic name meaning all available interfaces
 
 if paramMap['usage']:
     params.usage()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((listenAddr, listenPort))
-s.listen(1)              # allow only one outstanding request
+s.listen(1)  # allow only one outstanding request
 # s is a factory for connected sockets
 
-conn, addr = s.accept()  # wait until incoming connection request (and accept it)
-print('Connected by', addr)
-
 while True:
-    
+    conn, addr = s.accept() # wait until incoming connection request (and accept it)
+    if os.fork() == 0:      # child becomes server
+        print('Connected by', addr)
+
+        os.write(1, 'Enter your Message: \n'.encode())
+        message = os.read(1, 1000).decode()
+        message_handler.send_message(conn, message)
+        conn.shutdown(socket.SHUT_WR)
---
+++ framing-os/message_handler.py
@@ -0,0 +1,41 @@
+import socket
+
+
+# Sends a message through the given socket
+# Receives a socket, and a byte message
+def send_message(sock, message):
+    # Create byte array that contains message size in 4 bytes
+    msg_len = bytearray(len(message).to_bytes(4, "big"))
+
+    # Append message len to current message
+    message = msg_len + message
+    sock.sendall(message)
+
+
+def receive_message(sock) -> bytearray:
+    # Read msg_len consistently from socket
+    b_msg_len = bytearray()
+    while len(b_msg_len) < 4:
+        curr_msg_len = sock.recv(4 - len(b_msg_len))
+
+        # No data len obtained
+        if not b_msg_len:
+            return None
+
+        b_msg_len.extend(curr_msg_len)
+
+    # Unpack data len into an int
+    msg_len = int.from_bytes(b_msg_len, byteorder="big")
+
+    # Read data of the length of data_len
+    message = bytearray()
+    while len(message) < msg_len:
+        curr_msg = sock.recv(msg_len - len(message))
+
+        # No data obtained/exists
+        if not curr_msg:
+            return None
+
+        message.extend(curr_msg)  # Add to the array of message
+
+    return message
